afd646c9d738136eace52c8d6c14f341005d7aa6
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-07 14:40:19 +0700

    Updates

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./config/db");
 require("dotenv").config();
 
-const app = express();
-const port = process.env.DB_PORT;
+// const aboutRoutes = require("./routes/about");
+// const solusiRoutes = require("./routes/solusi");
+const newsletterRoutes = require("./routes/newsletter");
 
-app.use(cors());
+const app = express();
+const port = process.env.PORT;  
+
+app.use(cors()); 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("Hello dari Backend");
-})
+  res.json("Hello From Backend");
+});
 
-// app.get("/about", (req, res) => {
-//   const sql = "SELECT * FROM berdaya.about";
-//   db.query(sql, (err, data) => {
-//     if (err) return res.json(err);
-//     return res.json(data);
-//   })
-// })
+app.get("/solusi", (req, res) => {
+  const sql = "SELECT * FROM solusi";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+// app.use("/about", aboutRoutes);
+// app.use("/solusi", solusiRoutes);
+app.use("/newsletter", newsletterRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
