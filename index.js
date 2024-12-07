5b2bfcacb7ce1c5fdae23cde2147e87454fd7def
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-07 20:29:55 +0700

    Updates

--- index.js
+++ index.js
@@ -1,35 +1,62 @@
+const mysql2 = require("mysql2");
+const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
-const db = require("./config/db");
 require("dotenv").config();
 
-
 const app = express();
-const port = process.env.PORT || 3000;  
 
-app.use(cors()); 
+const db = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    ca: fs.readFileSync(process.env.SSL), 
+  },
+});
+
+app.use(cors());
 app.use(express.json());
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    process.exit(1);
-  }
-  console.log("Database connected successfully!");
-});
-
 app.get("/", (req, res) => {
-  res.json("Halo ini dari Backend Berdaya");
+  res.json("Hello From Backend Berdaya, we are ready!");
 });
 
-app.get("/solusi", (req, res) => {
-  const sql = "SELECT * FROM db_berdaya.solusi";
+app.get("/about", (req, res) => {
+  const sql = "SELECT * FROM about";
   db.query(sql, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to Azure MySQL:", err.message);
+//     throw err;
+//   }
+//   console.log("Connected to Azure MySQL!");
+
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to Azure MySQL:", err.message);
+      process.exit(1); 
+    }
+    console.log("Connected to Azure MySQL!");
+  });
+
+  db.query("SELECT * FROM about", (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err.message);
+    } else {
+      console.log("Query results:", results);
+    }
+    db.end();
+  });
+// });
+
+app.listen(process.env.DB_PORT, () => {
+  console.log(`Server is running on port ${process.env.DB_PORT}`);
+})
