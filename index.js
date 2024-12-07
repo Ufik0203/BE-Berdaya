787bd490268ef5724f75360787628b23dc1bcc98
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-07 13:39:42 +0700

    Updates

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const aboutRoutes = require("./routes/about");
-const solusiRoutes = require("./routes/solusi");
+// const aboutRoutes = require("./routes/about");
+// const solusiRoutes = require("./routes/solusi");
 const newsletterRoutes = require("./routes/newsletter");
 
 const app = express();
-const port = process.env.PORT || 8800;  
+const port = process.env.PORT;  
 
 app.use(cors()); 
 app.use(express.json());
@@ -16,8 +16,16 @@ app.get("/", (req, res) => {
   res.json("Hello From Backend");
 });
 
-app.use("/about", aboutRoutes);
-app.use("/solusi", solusiRoutes);
+app.get("/solusi", (req, res) => {
+  const sql = "SELECT * FROM solusi";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+// app.use("/about", aboutRoutes);
+// app.use("/solusi", solusiRoutes);
 app.use("/newsletter", newsletterRoutes);
 
 app.use((err, req, res, next) => {
