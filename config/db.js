a7719f79f581a9bfa212b6c2041ab04a7f476a28
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-07 20:51:54 +0700

    Updates

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-// const mysql2 = require("mysql2");
-// const fs = require("fs");
-// require("dotenv").config();
+const mysql2 = require("mysql2");
+const fs = require("fs");
+require("dotenv").config();
 
-// const db = mysql2.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER, 
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT || 3306,
-//     ssl: {
-//       ca: fs.readFileSync(process.env.SSL), // Path ke sertifikat SSL
-//     },
-//   });
+const db = mysql2.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+      ca: fs.readFileSync(process.env.SSL), // Path ke sertifikat SSL
+    },
+  });
 
-// module.exports = db;
+module.exports = db;
--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const mysql2 = require("mysql2");
-const fs = require("fs");
+// const mysql2 = require("mysql2");
+// const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 
-const db = mysql2.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    ca: fs.readFileSync(process.env.SSL), 
-  },
-});
+// const db = mysql2.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER, 
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+//   ssl: {
+//     ca: fs.readFileSync(process.env.SSL), 
+//   },
+// });
 
 app.use(cors());
 app.use(express.json());
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to Azure MySQL:", err.message);
-//     process.exit(1); 
-//   }
-//   console.log("Connected to Azure MySQL!");
-// });
 
 app.get("/", (req, res) => {
   res.json("Hello From Backend Berdaya, we are ready!");
@@ -47,7 +40,6 @@ app.get("/about", (req, res) => {
 //   }
 //   console.log("Connected to Azure MySQL!");
 
-
   // db.query("SELECT * FROM about", (err, results) => {
   //   if (err) {
   //     console.error("Error executing query:", err.message);
@@ -56,7 +48,6 @@ app.get("/about", (req, res) => {
   //   }
   //   db.end();
   // });
-// });
 
 app.listen(process.env.DB_PORT, () => {
   console.log(`Server is running on port ${process.env.DB_PORT}`);
