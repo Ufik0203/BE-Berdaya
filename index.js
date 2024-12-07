f71ed609ca11699c04b0923dc48ba1fde9072535
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-08 06:08:22 +0700

    Updates Object

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -28,6 +29,48 @@ app.get("/solusi", (req, res) => {
     return res.json(data);
   })
 })
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+app.post("/newsletter", (req, res) => {
+    const { email } = req.body;
+
+    const sql = "INSERT INTO subscribers_email (email) VALUES (?)";
+    db.query(sql, [email], (err, data) => {
+        if (err) {
+            console.error("Error saving email to database:", err);
+            return res.status(500).json({ message: "Gagal menyimpan email ke database" });
+        }
+
+        const mailOptions = {
+            from: {
+                name: "Berdaya",
+                address: process.env.EMAIL_USER,
+            },
+            to: email,
+            subject: "Terima kasih telah mengikuti Berdaya",
+            html: `<p>Terima kasih telah mengikuti Berdaya. Kami akan memberikan update perkembangan Berdaya secara berkala.</p>`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error("Error sending email:", error);
+                return res.status(500).json({ message: "Gagal mengirim email" });
+            }
+            // console.log("Email sent:", info.response);
+            return res.status(200).json({ message: "Success" });
+        });
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
