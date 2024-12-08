ad44bcd2f37a7b1d66f6210f3c420d8aebf7db34
ufik0203@gmail.com <ufik0203@gmail.com>
2024-12-08 07:21:34 +0700

    Object Implement

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
 const nodemailer = require("nodemailer");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
@@ -27,49 +28,68 @@ app.get("/solusi", (req, res) => {
   db.query(sql, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
-  })
-})
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: "smtp.gmail.com",
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  });
 });
 
-app.post("/newsletter", (req, res) => {
-    const { email } = req.body;
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-    const sql = "INSERT INTO subscribers_email (email) VALUES (?)";
-    db.query(sql, [email], (err, data) => {
+const newsLetterLimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Anda sudah melakukan request sebanyak 5 kali dalam waktu 15 menit, mohon tunggu sejenak untuk melakukan request lagi",
+});
+
+app.post("/newsletter", newsLetterLimit, (req, res) => {
+  const { email } = req.body;
+
+  const checkEmail = "SELECT * FROM subscribers_email WHERE email = ?";
+  db.query(checkEmail, [email], (err, data) => {
+    if (err) {
+      console.error("Error checking email:", err);
+      return res.status(500).json({ message: "Gagal memeriksa email yang terdaftar" });
+    }
+
+    if (data.length > 0) {
+      return res.status(400).json({
+        message: "Terimas kasih, Email andah sudah terdaftar sebelumnya",
+      });
+    }
+
+    const insertEmail = "INSERT INTO subscribers_email (email) VALUES (?)";
+    db.query(insertEmail, [email], (err) => {
+      if (err) {
+        console.error("Error saving email to database:", err);
+        return res.status(500).json({ message: "Gagal menyimpan email ke database" });
+      }
+
+      const mailOptions = {
+        from: {
+          name: "Berdaya",
+          address: process.env.EMAIL_USER,
+        },
+        to: email,
+        subject: "Terima kasih telah mengikuti Berdaya",
+        html: `<p>Terima kasih telah mengikuti Berdaya. Kami akan memberikan update perkembangan Berdaya secara berkala.</p>`,
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
         if (err) {
-            console.error("Error saving email to database:", err);
-            return res.status(500).json({ message: "Gagal menyimpan email ke database" });
+          console.error("Error sending email:", err);
+          return res.status(500).json({ message: "Gagal mengirim email" });
         }
-
-        const mailOptions = {
-            from: {
-                name: "Berdaya",
-                address: process.env.EMAIL_USER,
-            },
-            to: email,
-            subject: "Terima kasih telah mengikuti Berdaya",
-            html: `<p>Terima kasih telah mengikuti Berdaya. Kami akan memberikan update perkembangan Berdaya secara berkala.</p>`,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error("Error sending email:", error);
-                return res.status(500).json({ message: "Gagal mengirim email" });
-            }
-            // console.log("Email sent:", info.response);
-            return res.status(200).json({ message: "Success" });
-        });
+        return res.status(200).json({ message: "Success" });
+      });
     });
+  });
 });
 
 app.listen(port, () => {
